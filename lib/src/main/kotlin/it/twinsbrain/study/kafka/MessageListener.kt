8b39227473ca349aca43f2fdd4ca125adb01fc88
Paolo Sciarra <itbhps@gmail.com>
2022-01-30 20:20:46 +0100

    removed unused variables

--- lib/src/main/kotlin/it/twinsbrain/study/kafka/MessageListener.kt
+++ lib/src/main/kotlin/it/twinsbrain/study/kafka/MessageListener.kt
@@ -1,18 +1,11 @@
 package it.twinsbrain.study.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
 class MessageListener {
-
-  @Value("\${kafka.topic.name}")
-  lateinit var kafkaTopic: String
-
-  @Value("\${spring.kafka.consumer.group-id}")
-  lateinit var groupId: String
 
   @KafkaListener(id = "\${spring.kafka.consumer.group-id}", topics = ["\${kafka.topic.name}"])
   fun listen(record: ConsumerRecord<Int, String>) {
